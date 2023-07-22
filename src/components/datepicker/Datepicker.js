aabe6f34b3357480f235a87d4eec5114ce8d65e8
Mxtgy <maxime.tanguy22@gmail.com>
2023-07-22 21:49:15 +0900

    Ajout Datatables
    Ajout Datatables + Proptypes + Commentaires + Modifs

--- src/components/datepicker/Datepicker.js
+++ src/components/datepicker/Datepicker.js
@@ -1,5 +1,10 @@
-import './Datepicker.module.css';
+import './Datepicker.css';
+import PropTypes from 'prop-types';
 
+
+/*
+This is the datepicker component present on the homepage form.
+*/
 function Datepicker({ name, id }) {
 
     return (
@@ -8,6 +13,11 @@ function Datepicker({ name, id }) {
             <input className="datepicker" type="date" id={ id } />
         </>
     );
+}
+
+Datepicker.propTypes = {
+    name: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired
 }
 
 export default Datepicker;
--- src/components/select/Select.js
+++ src/components/select/Select.js
@@ -1,5 +1,10 @@
-import './Select.module.css';
+import './Select.css';
+import PropTypes from 'prop-types';
 
+
+/*
+This is the select component present on the homepage form.
+*/
 function Select({ id, tagname, title, content}) {
 
     var isContent = content;
@@ -18,6 +23,13 @@ function Select({ id, tagname, title, content}) {
             : null }
         </>
     );
+}
+
+Select.propTypes = {
+    id: PropTypes.string.isRequired,
+    tagname: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.array.isRequired
 }
 
 export default Select;
